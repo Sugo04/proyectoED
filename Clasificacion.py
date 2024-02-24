cac819468c23c452ee57e9645293f2b02583daea
Héctor Martín Ortega <hmarort075@g.educaand.es>
2024-02-24 13:07:49 +0100

    modified:   Clasificacion.py

--- Clasificacion.py
+++ Clasificacion.py
@@ -3,5 +3,6 @@
 #  Función que recibe la lista generada con la función anterior y la ordena según el número de puntos.
 
 def Clasificacion (datos):
-    datos.sort()
+    datos.sort(reverse=True)
     print("Lista ordenada")
+    return datos
