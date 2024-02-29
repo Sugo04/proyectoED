653a919c0ccecfc7a113b42f492745e5004bab5d
Héctor Martín Ortega <hmarort075@g.educaaand.es>
2024-02-29 08:37:01 +0100

    new file:   __pycache__/impClasificacion.cpython-312.pyc 	modified:   impClasificacion.py

--- impClasificacion.py
+++ impClasificacion.py
@@ -3,22 +3,18 @@
 # -Equipos(datosliga): Función que recibe la lista de diccionarios con los datos de la liga y devuelve un conjunto con los equipos de la liga.
 # -InfoEquipos(datosliga,equipos): Función que recibe la lista de diccionarios con los datos de la liga y el conjunto de equipos y devuelve una
 # lista de tuplas, en cada tupla se guarda un equipo con los partidos ganados, empatados y perdidos y los puntos obtenidos.
-
-def impClasificacion(equipos, infoEquipos):
-    # Eliminamos la cabecera si viene desde infoEquipos
-    if infoEquipos[0][0] == "Equipo":
-        infoEquipos = infoEquipos[1:]
-
-    # Ordenamos la lista de equipos por el número de puntos como en el ejercicio de listas
-    infoEquipos.sort(key=lambda x: x[4], reverse=True)
-
+import Equipos as e
+import InfoEquipos as ie
+def impClasificacion(liga):
+    equipos=e.Equipos(liga)
+    infoEquipos=ie.InfoEquipos(liga,equipos)
     #Imprimimos la nueva cabecera
     print("Posición | Equipo | Ganados | Empatados | Perdidos | Puntos ")
     print("---------|--------|---------|-----------|----------|--------")
     
     #Recorremos el rango de todos los equipos.
     for i in range(len(equipos)):
-        equipo = equipos[i]
+        equipo = infoEquipos[i][0]
         print("%2d | %15s | %2d | %2d | %2d | %2d" % (
             i + 1, equipo, infoEquipos[i][1], infoEquipos[i][2], infoEquipos[i][3], infoEquipos[i][4]))
 
